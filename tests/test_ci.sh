b16420e0dbda82a7dbbe20bfcfe2c2da9229013b
jonathanagustin <jonathanagustin@gmail.com>
2023-09-15 07:38:23 +0000

    update test script

--- tests/test_ci.sh
+++ tests/test_ci.sh
@@ -1,7 +1,30 @@
 #!/usr/bin/env bash
 
-# Change to the parent directory
-cd ..
+# Search for the target directory upwards
+search_for_directory() {
+    local target_dir="$1"
+    local max_attempts="$2"
+    local current_dir="$PWD"
+    local attempts=0
+
+    while [[ $attempts -lt $max_attempts ]]; do
+        if [[ -d "$current_dir/$target_dir" ]]; then
+            echo "Found $target_dir at $current_dir"
+            cd "$current_dir/$target_dir"
+            return 0
+        fi
+
+        # Move up one directory
+        current_dir=$(dirname "$current_dir")
+        ((attempts++))
+    done
+
+    echo "$target_dir not found after $max_attempts attempts."
+    return 1
+}
+
+# Search for the directory and change to it if found
+search_for_directory "light-distribution-analysis" 10
 
 # Check if act is installed
 if ! [ -f ./bin/act ]; then
@@ -13,7 +36,7 @@ else
 fi
 
 # Create .actrc file with default configuration
-echo "-P ubuntu-latest=catthehacker/ubuntu:act-22.04" > .actrc
+echo "-P ubuntu-latest=nektos/act-environments-ubuntu:18.04" > .actrc
 
 # Execute act
 ./bin/act 2>&1 | tee log.txt
